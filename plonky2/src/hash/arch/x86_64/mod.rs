fa6a7f9a7ff41a3fb3e72453abcac0df3610d609
lighter-zz <allaboutshop8@163.com>
2025-11-07 19:37:29 -0500

    Update mod.rs

--- plonky2/src/hash/arch/x86_64/mod.rs
+++ plonky2/src/hash/arch/x86_64/mod.rs
@@ -3,3 +3,7 @@
 // // - BMI2 (for MULX and SHRX)
 // #[cfg(all(target_feature = "avx2", target_feature = "bmi2"))]
 // pub(crate) mod poseidon_goldilocks_avx2_bmi2;
+#[cfg(target_feature = "avx2")]
+pub mod goldilocks_avx2;
+#[cfg(target_feature = "avx2")]
+pub mod poseidon2_goldilocks_avx2;
